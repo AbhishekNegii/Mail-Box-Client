ca0c5b01e3663ddd3e00e98cef6fe809a9be2d0b
Abhisheknegii <abhibaba007@gmail.com>
2022-09-27 22:51:59 +0530

    Home page link with auth form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import AuthForm from './Pages/AuthForm';
+import HomePage from './Pages/HomePage';
+import {Redirect,Switch,Route} from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isLoggedIn=useSelector(state=>state.auth.isLoggedIn)
+  console.log("isLog",isLoggedIn)
   return (
-    <div className="App">
-     <h1>Mail Chat Box</h1>
-     <AuthForm/>
-    </div>
+    <Switch>
+      <Route path="/authform">
+        <AuthForm/>
+        </Route>
+        {isLoggedIn && <Route path="/homepage" exact>
+          <HomePage/>
+        </Route>}
+        <Route path="*">
+          <Redirect to="/authform">
+            <AuthForm />
+          </Redirect>
+          </Route>
+    </Switch>
   );
 }
 
---
+++ src/Links/Links.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Links = () => {
+  return (
+    <div>
+        <Link to="/homepage"></Link>
+        <Link to="/authform"></Link>
+    </div>
+  )
+}
+
+export default Links
